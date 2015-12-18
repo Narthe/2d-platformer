fae4e6eac3659f5177a8f07d2719ba7ed89f8819
Narthe <sismikkk@hotmail.fr>
2015-12-18 22:59:15 +0100

    wip enemy AI

---
+++ Assets/EnemyAI.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using Pathfinding;
+
+[RequireComponent (typeof(Rigidbody2D))]
+[RequireComponent(typeof(Seeker))]
+public class EnemyAI : MonoBehaviour {
+
+    public Transform target;
+    public float updateRate = 2f;
+
+    private Seeker seeker;
+    private Rigidbody2D rb;
+
+    public Path path;
+
+    public float speed = 300f;
+    public ForceMode2D fMode;
+
+    [HideInInspector]
+    public bool pathIsEnded = false;
+
+    public float nextWaypointDistance = 3f;
+}
